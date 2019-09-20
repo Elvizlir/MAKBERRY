ae30ddb01bc459ffe424c06cb29432860d447c27
Roman Makeev <elvizlir@gmail.com>
2019-09-20 15:33:34 +0300

    product-page

--- public/js/common.js
+++ public/js/common.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function ($) {
 
 	JSCCommon.CustomInputFile(); 
 	// добавляет подложку для pixel perfect
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/screen.png);"></div>')
+	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/goodscard.psd.png);"></div>')
 	// /добавляет подложку для pixel perfect
 
 
